c5709220bc56067db2ecc01e033bb2adcf5e3b04
Md Arfan Khan <khan188993@gmail.com>
2022-02-13 20:53:29 +0600

    pin Matcher Project done

---
+++ pin-matcher-master/note.js
@@ -0,0 +1,10 @@
+/* 
+*)generate random pin by clicking , 
+*)calc btn click krle input e show krbe , ekta ekta clear and single delete 
+*)submit btn click krle pin mathc hole seta na hole onno ta text show krbo 
+*)submit ekta krle trail ekta kore namabo ,
+
+
+*/
+
+
